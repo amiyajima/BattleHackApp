b7743ef9938430e6425e7d49a5fb65dae8f5b844
Thomas <tzl2@duke.edu>
2015-04-18 22:46:49 -0400

    Hide Keyboard

--- app/src/main/java/com/example/annamiyajima/mapstest/MainActivity.java
+++ app/src/main/java/com/example/annamiyajima/mapstest/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,20 +17,40 @@ public class MainActivity extends ActionBarActivity {
     private Button findbutton;
     private Button searchlocation;
     private Button addtime;
-    private EditText findEditText;
+    private EditText find;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findEditText = (EditText) findViewById(R.id.findeditText);
+
+        find = (EditText) findViewById(R.id.findeditText);
         findbutton = (Button) findViewById(R.id.findbutton);
         searchlocation = (Button) findViewById(R.id.searchbutton);
         addtime = (Button) findViewById(R.id.addtimebutton);
 
         addOnClickSearchLocation();
+        hideKeyboardMain();
         addOnClickAdd();
     }
+
+    public void hideKeyboardMain(){
+        find.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+    }
+    //helper method to hideKeyboardMain()
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    //Button link to map page
     public void addOnClickSearchLocation(){
         final Context context = this;
 
@@ -40,9 +61,9 @@ public class MainActivity extends ActionBarActivity {
                 startActivity(intent);
             }
         });
-
     }
 
+    //button link to add time page
     public void addOnClickAdd(){
         final Context context = this;
         addtime.setOnClickListener(new View.OnClickListener(){
